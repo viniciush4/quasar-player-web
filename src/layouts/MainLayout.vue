922f807b1bdaded0b09690316a0cf0a8f814cf76
Vinícius Berger <viniciush4@gmail.com>
2020-04-19 21:05:22 -0300

    Primeiro commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,100 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-grey-1">
+      <q-toolbar class="q-py-lg q-px-xl">
+        <q-btn flat label="Music" color="primary"/>
+        <q-btn flat label="Podcast" color="black" />
+        <q-btn flat label="Live" color="black"/>
+        <q-space />
+        <q-input dense color="lime-11" outlined placeholder="Type here to search">
+          <template v-slot:prepend>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+        <q-space />
+        <q-btn flat round dense icon="notifications" color="black" class="q-mr-md" size="sm">
+          <div style="border-radius: 50%; width: 10px; height: 10px;" class="q-badge--floating bg-primary"></div>
+        </q-btn>
+        <q-btn flat round dense icon="settings" color="black" class="q-mr-lg" size="sm" />
+        <div class="flex bg-grey-9 rounded-borders items-center">
+          <div class="bg-grey-8 q-pa-sm rounded-borders">
+            <q-avatar rounded size="sm">
+              <img src="/statics/avatar.jpeg">
+            </q-avatar>
+          </div>
+          <div class="text-white q-pa-sm text-bold text-caption">
+            Vinicius Berger
+          </div>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      show-if-above
+      content-class="bg-grey-3"
+    >
+      <div class="text-h6 q-px-xl q-py-lg">
+        Groovvy
+      </div>
+      <q-list class="q-py-lg">
+        <q-item-label
+          header
+          class="text-grey-8 q-px-xl text-uppercase"
+        >
+          Menu
+        </q-item-label>
+
+        <q-item clickable v-ripple class="q-px-xl">
+          <q-item-section avatar>
+            <q-icon color="primary" name="bluetooth" size="xs" />
+          </q-item-section>
+          <q-item-section>Explore</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple class="q-px-xl">
+          <q-item-section avatar>
+            <q-icon color="primary" name="build" size="xs" />
+          </q-item-section>
+          <q-item-section>Genres</q-item-section>
+        </q-item>
+      </q-list>
+      <q-list class="q-pt-lg">
+        <q-item-label
+          header
+          class="text-grey-8 q-px-xl text-uppercase"
+        >
+          Library
+        </q-item-label>
+
+        <q-item clickable v-ripple class="q-px-xl">
+          <q-item-section avatar>
+            <q-icon color="primary" name="bluetooth" size="xs" />
+          </q-item-section>
+          <q-item-section>Explore</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple class="q-px-xl">
+          <q-item-section avatar>
+            <q-icon color="primary" name="build" size="xs" />
+          </q-item-section>
+          <q-item-section>Genres</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-page style="background: linear-gradient(180deg, rgba(250,250,250,1) 0%, rgba(245,245,245,1) 50%);">
+    <div class="row text-h6 q-px-xl q-py-xl">
+      Trending New Hits
+    </div>
+    <div class="row column text-h1 q-px-xl">
+      <div class="row text-h1 text-bold q-pl-lg">
+        The Havanna
+      </div>
+      <div class="row text-h4 text-bold q-pl-lg q-pt-sm">
+        Camila Cabello
+      </div>
+      <div class="row q-pl-lg q-pt-xl">
+        <q-btn rounded unelevated no-caps color="primary" label="Listen Now" size="lg"/>
+        <q-btn round outline color="primary" icon="favorite" size="lg" class="q-ml-lg"/>
+      </div>
+    </div>
+    <div class="row q-pt-xl">
+      <div class="col-8">
+        <q-card flat class="bg-white q-my-md q-ml-xl q-mr-md">
+          <q-card-section>
+            <div class="row items-center no-wrap">
+              <div class="col">
+                <div class="text-h6">Top Artists</div>
+              </div>
+              <div class="col-auto">
+                <div class="text-caption">
+                  See all
+                </div>
+              </div>
+            </div>
+          </q-card-section>
+
+          <q-card-section>
+            teste
+          </q-card-section>
+
+        </q-card>
+
+        <q-card flat class="bg-white q-my-md q-ml-xl q-mr-md">
+          <q-card-section>
+            <div class="row items-center no-wrap">
+              <div class="col">
+                <div class="text-h6">Top Artists</div>
+              </div>
+              <div class="col-auto">
+                <div class="text-caption">
+                  See all
+                </div>
+              </div>
+            </div>
+          </q-card-section>
+
+          <q-card-section>
+            teste
+          </q-card-section>
+
+        </q-card>
+      </div>
+      <div class="col-4">
+        <q-card flat class="bg-white q-my-md q-ml-md q-mr-xl">
+          <q-card-section>
+            <div class="row items-center no-wrap">
+              <div class="col">
+                <div class="text-h6">Player</div>
+              </div>
+              <div class="col-auto">
+                <div class="text-caption">
+                  <q-btn flat icon="favorite" color="black" />
+                </div>
+              </div>
+            </div>
+          </q-card-section>
+
+          <q-card-section>
+            teste <br>
+            teste <br>
+            teste <br>
+            teste <br>
+            teste <br>
+            teste <br>
+            teste <br>
+            teste <br>
+            teste <br>
+          </q-card-section>
+
+        </q-card>
+      </div>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
